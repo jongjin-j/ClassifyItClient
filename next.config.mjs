28f775de405924f10d2d98bc22d9873ecd3bbc1e
jongjin-j <jjjung3571@gmail.com>
2024-11-18 17:18:07 -0500

    Changing destination to deployed flask app

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/:path*',
-        destination: 'http://127.0.0.1:5000/:path*'
+        source: "/:path*",
+        destination: "https://classifyitserver.onrender.com/:path*",
       },
     ];
   },
